6f9f9a29155245648aa263bf40bd5a3f3f36ca55
bipin24746 <102451706+bipin24746@users.noreply.github.com>
2023-09-26 15:19:26 +0545

    Add files via upload

---
+++ news/forgotPassword/mailer.php
@@ -0,0 +1,42 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// require 'phpmailer/src/Exception.php';
+// require 'phpmailer/src/PHPMailer.php';
+// require 'phpmailer/src/SMTP.php';
+
+require __DIR__ . "/vendor/autoload.php";
+
+$mail = new PHPMailer(true);
+
+// Enable verbose debug output for troubleshooting
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+// Set the SMTP server for Gmail
+$mail->Host = "smtp.gmail.com";
+
+// Enable TLS encryption
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+
+// Set the SMTP port for Gmail
+$mail->Port = 587;
+
+// Your Gmail address (sender)
+$mail->Username = "sainjubipin247460@gmail.com"; // Replace with your Gmail address
+
+// Use the generated App Password for Gmail
+$password = "lyyyecmtzwunvzhs";
+$mail->Password = $password; // Replace with your App Password
+
+// Set the email format to HTML
+$mail->isHTML(true);
+
+// Return the $mail object
+return $mail;
+?>
